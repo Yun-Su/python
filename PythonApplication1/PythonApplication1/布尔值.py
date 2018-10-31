61a48b38bd906211c80d8a165a905aa3f426bc9c
Yun_su <Stack_Overflow_Error@outlook.com>
2018-10-31 20:26:24 +0800

    d

---
+++ PythonApplication1/PythonApplication1/布尔值.py
@@ -0,0 +1,12 @@
+
+True and True
+True and False
+False and False
+5 > 3 and 3 > 1
+True or True
+True or False
+False or False
+5 > 3 or 1 > 3
+not True
+not False
+not 1 > 2
---
+++ PythonApplication1/PythonApplication1/布尔判断.py
@@ -0,0 +1,7 @@
+a = 123 # a是整数
+print(a)
+a = 'ABC' # a变为字符串
+print(a)
+a=True #a变为布尔值
+#以上为动态赋值
+
