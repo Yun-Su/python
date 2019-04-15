d9a27cbbee03c6a59cfdd062784596eb8e8c39ec
YunSu <Stack_Overflow_Error@outlook.com>
2019-04-15 21:19:44 +0800

    类的实例

--- PythonApplication1/PythonApplication1/类的实例.py
+++ PythonApplication1/PythonApplication1/类的实例.py
@@ -1,7 +1,9 @@
-class Test:
+class Test: #通过class来定义类 Test为类名 
+    #通常格式 class test(obj):  (obj)意为当前test类是从obj这个类中继承的
     def prt(self):
         print(self)
         print(self.__class__)
 t=Test()
 t.prt()
-
+#类的方法和普通函数区别在于有个第一参数名称,通常是self
+#self 代表的是类的实例，代表当前对象的地址，而 self.class 则指向类
